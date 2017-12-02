a13390d81586da06fea7ef1f4c621a14b9cbd222
Heather Rae <heatherrae150@yahoo.co.uk>
2017-12-02 21:09:45 +0000

    Add prime table file and specs folder.  Install micha for unit testing

---
+++ models/prime_tables.js
@@ -0,0 +1,5 @@
+var PrimeTable = function() {
+  this.primes = []
+}
+
+module.exports = PrimeTable
---
+++ models/specs/prime_tables_specs.js
@@ -0,0 +1,12 @@
+var assert = require("assert")
+var PrimeTable = require("../prime_tables.js")
+
+describe("Prime Table", function() {
+  var primeTable
+
+  beforeEach(function() {
+    primeTable = new PrimeTable()
+  })
+
+  
+})
