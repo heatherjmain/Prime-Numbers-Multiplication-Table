60b9111cfa92769721404e573c416ad41b1c0eb2
Heather Rae <heatherrae150@yahoo.co.uk>
2017-12-02 22:05:18 +0000

    Add function to determine if a given int is a prime number

--- models/prime_tables.js
+++ models/prime_tables.js
@@ -2,4 +2,20 @@ var PrimeTable = function() {
   this.primes = []
 }
 
+// helper function to determine whether n is prime
+PrimeTable.prototype.isPrime = function(n) {
+  // divide n by each number smaller than n
+  // no need to include 1 - start at 2
+  // no need to include n
+  let i
+
+  for (i = 2; i < n; i++) {
+    if (n % i === 0) {
+      // if divisible by any other number then not prime
+      return false
+    }
+  }
+  return true
+}
+
 module.exports = PrimeTable
--- models/specs/prime_tables_specs.js
+++ models/specs/prime_tables_specs.js
@@ -12,5 +12,30 @@ describe("Prime Table", function() {
     assert.strictEqual(0, primeTable.primes.length)
   })
 
+  it("should be able to check a number to dentermine if prime when given prime number 2", function() {
+    assert.strictEqual(true, primeTable.isPrime(2))
+  })
+
+  it("should be able to check a number to dentermine if prime when given prime number 13", function() {
+    assert.strictEqual(true, primeTable.isPrime(13))
+  })
+
+  it("should be able to check a number to dentermine if prime when given prime number 113", function() {
+    assert.strictEqual(true, primeTable.isPrime(113))
+  })
+
+  it("should be able to check a number to dentermine if prime when given a non prime number 25", function() {
+    assert.strictEqual(false, primeTable.isPrime(25))
+  })
+
+  it("should be able to check a number to dentermine if prime when given a non prime number 100", function() {
+    assert.strictEqual(false, primeTable.isPrime(100))
+  })
+
+  it("should be able to check a number to dentermine if prime when given a non prime number 333", function() {
+    assert.strictEqual(false, primeTable.isPrime(333))
+  })
+
+
 
 })
