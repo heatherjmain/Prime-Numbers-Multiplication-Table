27f9927436b7a3a996c262c27ef47e139b7a08f5
Heather Rae <heatherrae150@yahoo.co.uk>
2017-12-03 10:38:02 +0000

    Render multiplication table on button click and user input

--- client/build/bundle.js
+++ client/build/bundle.js
@@ -73,15 +73,30 @@ window.addEventListener("load", function() {
   var run = new displayPrimeTable()
 
   displayPrimeTable.prototype.render = function() {
-    var button = document.getElementById("submit-button")
+    let button = document.getElementById("submit-button")
 
     button.addEventListener("click", function() {
-      var userInputField = document.getElementById("user-input")
-      var requestedNumber = parseInt(userInputField.value)
+      let userInputField = document.getElementById("user-input")
+      let requestedNumber = parseInt(userInputField.value)
       // console.log("user input", requestedNumber)
 
-      var getTable = run.makeGrid(requestedNumber)
-      console.log("table", getTable)
+      let table = run.makeGrid(requestedNumber)
+      // console.log("table", table)
+      for (let row of table) {
+        let tableWrapper = document.getElementById("table-wrapper")
+        let table = document.createElement("table")
+        let tableRow = document.createElement("tr")
+
+        for (element of row) {
+          let number = document.createElement("td")
+          let value = document.createTextNode(element)
+          number.appendChild(value)
+          tableRow.appendChild(number)
+          table.appendChild(tableRow)
+          tableWrapper.appendChild(table)
+        }
+
+      }
     })
   }
   run.render()
